d70326c76bffbf4c3baba4d149653ac08415db23
maazahmedse <maazahmedca@gmail.com>
2025-09-19 15:08:30 -0400

    Setting up the project

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -3,8 +3,7 @@ import { useEffect } from "react";
 
 function HomePage() {
 
-    return "<h1>Welcome to first page of the application. Welcome to the world of Art.</h1>"
+    return "<h1>Welcome to first page of the application.</h1>"
 }
-
 
 export default HomePage;
